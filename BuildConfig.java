003cb9343dfb08472b5cf6c818d1b4b4a014cfc5
TheWrathOfOdin <67510468+TheWrathOfOdin@users.noreply.github.com>
2020-11-19 11:10:02 -0500

    Create BuildConfig.java

---
+++ BuildConfig.java
@@ -0,0 +1,5 @@
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "org.tensorflow.lite.examples.classification.tflite";
+  public static final String BUILD_TYPE = "debug";
+}
